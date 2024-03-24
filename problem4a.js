60e2a9eba0be87ed6c808187e2be4344584aa741
Chandra Kusuma <77940961+CzNime@users.noreply.github.com>
2024-03-24 22:36:09 +0800

    Resolve problem4 (not done yet)

---
+++ problem4a.js
@@ -0,0 +1,17 @@
+function reverseing(n) {
+  return n.split("").reverse().join("");
+}
+
+// for (let i = 0; i <= 100; i++) {
+//   if (angka < i * i) {
+//     angka = i * i;
+//   }
+// }
+
+// let bil1 = [1, 2, 3];
+
+// // let bil2 = bil1.split("").reverse().join("");
+// let bil3 = bil1.reverse();
+// console.log(bil3, bil1);
+
+console.log(reverseing("asdas"));
