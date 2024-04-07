099594080158c6d196bd96ad50f1b64510937368
bangcan <intrusted01@gmail.com>
2024-04-07 13:20:02 +0800

    triangle number to have over 500divisors fixed

--- problem12a.js
+++ problem12a.js
@@ -1,36 +1,28 @@
 // If we list all the natural numbers below
 // that are multiples of 3 or 5, we get 3,5,6 and 9. The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
 
-let lengthDivisor = 200;
+let lengthDivisor = 500;
 let indexSetigita = 1;
 let bilSegitiga = 0;
 let indexDivisor = 0;
 let kondisi = true;
 
-while (indexDivisor < lengthDivisor) {
+while (true) {  
   indexDivisor = 0;
   bilSegitiga = (indexSetigita * (indexSetigita + 1)) / 2;
-  for (let i = 1; i <= bilSegitiga; i++) {
-    if (bilSegitiga % i == 0) {
-      console.log(bilSegitiga, " ", indexSetigita, "  ", indexDivisor, i);
-      if (i == bilSegitiga) {
-        // faktorBilSegiitiga.push(i);
+  for (let i = 1; i <= Math.sqrt(bilSegitiga); i++) {  
+    if (bilSegitiga % i === 0) {
+      if (i * i === bilSegitiga) {  
         indexDivisor += 1;
-        break;
+      } else {
+        indexDivisor += 2; 
       }
-      // faktorBilSegiitiga.push(i);
-      indexDivisor += 1;
     }
   }
-  // console.log(faktorBilSegiitiga);
+  if (indexDivisor > lengthDivisor) {
+    break;  
+  }
   indexSetigita++;
 }
 
-console.log(indexDivisor);
-// console.log(faktorBilSegiitiga);
-// console.log(faktorBilSegiitiga.length);
-
-// console.log("=====================+++++++========================");
-// faktorBilSegiitiga.forEach((angka) => {
-//   console.log(angka);
-// });
+console.log(bilSegitiga); 
